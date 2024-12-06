24a5bfc0ca3b184563db3cd574424d618a399dec
Git'Fellow <12234510+solracsf@users.noreply.github.com>
2024-12-06 19:25:32 +0100

    feat(occ): Better handling of disabled appstore
    Signed-off-by: Git'Fellow <12234510+solracsf@users.noreply.github.com>

--- core/Command/App/Update.php
+++ core/Command/App/Update.php
@@ -10,6 +10,7 @@ namespace OC\Core\Command\App;
 
 use OC\Installer;
 use OCP\App\IAppManager;
+use OCP\IConfig;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -18,8 +19,11 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Update extends Command {
+	public const APP_STORE_URL = 'https://apps.nextcloud.com/api/v1';
+
 	public function __construct(
 		protected IAppManager $manager,
+		protected IConfig $config,
 		private Installer $installer,
 		private LoggerInterface $logger,
 	) {
@@ -57,6 +61,20 @@ class Update extends Command {
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$appStoreEnabled = $this->config->getSystemValueBool('appstoreenabled', true);
+		$internetAvailable = $this->config->getSystemValueBool('has_internet_connection', true);
+		$isDefaultAppStore = $this->config->getSystemValueString('appstoreurl', self::APP_STORE_URL) === self::APP_STORE_URL;
+
+		if (!$appStoreEnabled) {
+			$output->writeln('App store access is disabled');
+			return 1;
+		}
+
+		if (!$internetAvailable && $isDefaultAppStore) {
+			$output->writeln('This instance has no internet connection to access the default App store');
+			return 1;
+		}
+
 		$singleAppId = $input->getArgument('app-id');
 		$updateFound = false;
 
